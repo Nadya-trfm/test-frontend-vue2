d469f8adad8cd97f8ea1dc64f0e0878a6e0adb18
nadezhda <n.trofimova@monodigital.ru>
2024-05-05 14:42:30 +0300

    add bootstrap css

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import store from "./store";
 //Main pages
 import App from './views/App.vue';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = new Vue({
     el: '#app',
